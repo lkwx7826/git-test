9e5697dc157a46e8f5c5564fccc51fdea2a7b557
WangKe <1046653233@qq.com>
2019-09-05 21:44:44 +0800

    把本地工程添加到git

---
+++ GitDemo/src/com/itcast/git/GitDemo.java
@@ -0,0 +1,19 @@
+package com.itcast.git;
+
+/**
+ * @ClassName GitDemo
+ * @Description ToDo
+ * @Author WangKe
+ * @Date 2019/9/5 21:35
+ * @Version 1.0
+ **/
+
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!");
+        System.out.println("Hello Git!!!");
+        System.out.println("Hello Git!!!");
+        System.out.println("Hello Git!!!");
+        System.out.println("Hello Git!!!");
+    }
+}
